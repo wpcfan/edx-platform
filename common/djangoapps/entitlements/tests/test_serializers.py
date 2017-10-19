c408c030837b021e820f56c796720d9de32ed0d1
Albert St. Aubin <astaubin@edx.org>
2017-10-19 09:27:36 -0400

    Added Entitlement API
    [LEARNER-2661]
    The Entitlement API will allow for GET, POST, DELETE(Revoke), and
    variations of these methods of managing and retrieving data about
    Learner Entitlements.

---
+++ common/djangoapps/entitlements/tests/test_serializers.py
@@ -0,0 +1,26 @@
+from django.test import RequestFactory
+from xmodule.modulestore.tests.django_utils import ModuleStoreTestCase
+
+from entitlements.serializers import CourseEntitlementSerializer
+from entitlements.tests.factories import CourseEntitlementFactory
+
+
+class EntitlementsSerializerTests(ModuleStoreTestCase):
+
+    def test_data(self):
+        entitlement = CourseEntitlementFactory()
+        request = RequestFactory().get('')
+        serializer = CourseEntitlementSerializer(entitlement, context={'request': request})
+
+        expected = {
+            'user': entitlement.user.username,
+            'uuid': str(entitlement.uuid),
+            'expired_at': entitlement.expired_at,
+            'course_uuid': str(entitlement.course_uuid),
+            'mode': entitlement.mode,
+            'order_number': entitlement.order_number,
+            'created': entitlement.created.strftime('%Y-%m-%dT%H:%M:%S.%fZ'),
+            'modified': entitlement.modified.strftime('%Y-%m-%dT%H:%M:%S.%fZ'),
+        }
+
+        assert serializer.data == expected
