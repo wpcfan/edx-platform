31f15617facaf8bff2fc5f4494fa1975c79009a9
Waheed Ahmed <waheed.ahmed@arbisoft.com>
2017-11-06 20:01:50 +0500

    Implement program listing fragment for mobile app.
    Converted the program listing logic into a web fragment view
    and exposed a URL for mobile app.
    
    LEARNER-2980

--- lms/djangoapps/learner_dashboard/urls.py
+++ lms/djangoapps/learner_dashboard/urls.py
@@ -1,9 +1,10 @@
 """Learner dashboard URL routing configuration"""
 from django.conf.urls import url
 
-from . import views
+from . import programs, views
 
 urlpatterns = [
     url(r'^programs/$', views.program_listing, name='program_listing_view'),
+    url(r'^programs/fragment/$', programs.ProgramsFragmentView.as_view(), name='program_listing_fragment'),
     url(r'^programs/(?P<program_uuid>[0-9a-f-]+)/$', views.program_details, name='program_details_view'),
 ]
