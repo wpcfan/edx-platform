97c65b353a4cc2ba5b0b824c6477c726fd4c81b6
uzairr <uzairr@yahoo.com>
2017-11-07 10:42:19 +0000

    Create program detail fragment for mobile app
    To extract the program detail information edX mobile team has to
    parse all the un-necassary details which takes time that ultimately
    slows down app.To avoid it,a detail fragment is created that will
    be used in the mobile app as well as on the web.
    
    LEARNER-2981

--- lms/djangoapps/learner_dashboard/urls.py
+++ lms/djangoapps/learner_dashboard/urls.py
@@ -7,4 +7,6 @@ urlpatterns = [
     url(r'^programs/$', views.program_listing, name='program_listing_view'),
     url(r'^programs/fragment/$', programs.ProgramsFragmentView.as_view(), name='program_listing_fragment'),
     url(r'^programs/(?P<program_uuid>[0-9a-f-]+)/$', views.program_details, name='program_details_view'),
+    url(r'^programs/(?P<program_uuid>[0-9a-f-]+)/fragment/$', programs.ProgramDetailsFragmentView.as_view(),
+        name='program_detail_fragment'),
 ]
