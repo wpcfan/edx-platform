ea3ceeaba91feebb18a6cfd9d9edec344e82bb5e
Michael Youngstrom <myoungstrom@edx.org>
2017-11-07 15:15:39 -0500

    dont require tests to generate coveragerc files

--- pavelib/bok_choy.py
+++ pavelib/bok_choy.py
@@ -81,7 +81,6 @@ def test_a11y(options, passthrough_options):
     # Modify the options object directly, so that any subsequently called tasks
     # that share with this task get the modified options
     options.test_a11y.report_dir = Env.BOK_CHOY_A11Y_REPORT_DIR
-    options.test_a11y.coveragerc = Env.BOK_CHOY_A11Y_COVERAGERC
     options.test_a11y.extra_args = options.get('extra_args', '') + ' -a "a11y" '
     run_bokchoy(options.test_a11y, passthrough_options)
 
@@ -120,7 +119,6 @@ def pa11ycrawler(options, passthrough_options):
     # Modify the options object directly, so that any subsequently called tasks
     # that share with this task get the modified options
     options.pa11ycrawler.report_dir = Env.PA11YCRAWLER_REPORT_DIR
-    options.pa11ycrawler.coveragerc = Env.PA11YCRAWLER_COVERAGERC
     options.pa11ycrawler.should_fetch_course = getattr(
         options,
         'should_fetch_course',
